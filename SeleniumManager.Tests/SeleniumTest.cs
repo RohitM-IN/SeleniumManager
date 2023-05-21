24241a4092685f21c83e95685af59f028b5b06ff
RohitM-IN <rohit.mahajan.in@gmail.com>
2023-05-22 01:05:44 +0530

    update

---
+++ SeleniumManager.Tests/SeleniumTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SeleniumManager.Tests
+{
+    [TestClass]
+    public class SeleniumTest
+    {
+        [TestMethod]
+        public void TestSelenium()
+        {
+            //TODO
+        }
+
+        [TestMethod]
+        public void ParallelTestSelenium()
+        {
+            //TODO
+        }
+    }
+}
